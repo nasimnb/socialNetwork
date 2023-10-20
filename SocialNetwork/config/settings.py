596aa39da0755a5a77ff2a4bb364dd8004bcf13e
Nasim Nasiri <nasirinasim77@gmail.com>
2023-10-20 16:50:40 +0330

    Update settings.py

--- SocialNetwork/config/settings.py
+++ SocialNetwork/config/settings.py
@@ -135,11 +135,4 @@ LOGIN_URL = '/account/login/'
 
 
 
-#Google Account
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'zeynabnb8421@gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = 'rpxjhiafhimywumj'
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'NASA website'
+
